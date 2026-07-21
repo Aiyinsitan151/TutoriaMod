d6dac3bc768578e536e4679d4b71a8c740cb304a
leina <2381619296@qq.com>
2026-07-21 22:47:41 +0800

    把物品注册器添加到总线

--- src/main/java/org/nothink/tutoriamod/Tutoriamod.java
+++ src/main/java/org/nothink/tutoriamod/Tutoriamod.java
@@ -3,17 +3,19 @@ package org.nothink.tutoriamod;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.nothink.tutoriamod.init.ModItems;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(Tutoriamod.MODID)
+@Mod(Tutoriamod.MOD_ID)
 public class Tutoriamod {
 
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "tutoriamod";
+    public static final String MOD_ID = "tutoriamod";
 
     public Tutoriamod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.ITEMS.register(modEventBus);
 
     }
 }
